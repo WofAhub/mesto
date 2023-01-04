786c7929d0b8936830d52b91270893c0a351a144
WofA <wofamyworld@gmail.com>
2023-01-04 03:54:58 +0300

    1.04.2023 3:54pm

---
+++ pages/script.js
@@ -0,0 +1,35 @@
+// LET
+// кнопки
+let editbutton = document.querySelector('.button_profile_edit-button');
+let closebutton = document.querySelector('.button_pop-up_close-button');
+let savebutton = document.querySelector('.button_pop-up_save-button');
+// profile
+let profileinfo = document.querySelector('.profile__info');
+let profilename = document.querySelector('.profile__name');
+let profiledescription = document.querySelector('.profile__description');
+// element
+let likebutton = document.querySelector('.button_element_like-button');
+// pop-up
+let popup = document.querySelector('.pop-up');
+let editname = document.querySelector('.pop-up__container_field_name-edit');
+let editdescription = document.querySelector('.pop-up__container_field_description-edit');
+let popupfield = document.querySelector('.pop-up__container_field');
+
+// FUNCTIONS
+// POP-UP
+// активация-дизактивация попапа
+editbutton.addEventListener('click', function(){
+  popup.classList.add('pop-up_type_active');
+});
+closebutton.addEventListener('click', function(){
+  popup.classList.remove('pop-up_type_active');
+});
+// сохранение
+savebutton.addEventListener('click', function(){
+  profilename.textContent = editname.value;
+  profiledescription.textContent = editdescription.value;
+
+  profilename.value = '';
+  profiledescription.value = '';
+  
+});
