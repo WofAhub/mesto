8788a2004cdf1165239cda2c6400314a33bdc374
WofA <wofamyworld@gmail.com>
2023-01-06 22:15:47 +0300

    После проверки ревьювера

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,44 +1,43 @@
 // LET
 // кнопки
-let editButton = document.querySelector('.button_type_edit-button');
-let closeButton = document.querySelector('.button_type_close-button');
-let saveButton = document.querySelector('.button_type_save-button');
-let likeButton = document.querySelector('.button_type_like-button');
+let editButton = document.querySelector(".button_type_edit-button");
+let closeButton = document.querySelector(".button_type_close-button");
+let saveButton = document.querySelector(".button_type_save-button");
+let likeButton = document.querySelector(".button_type_like-button");
 // profile
-let profileInfo = document.querySelector('.profile__info');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
+let profileInfo = document.querySelector(".profile__info");
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
 // pop-up
-let popUp = document.querySelector('.pop-up');
-let descriptionEdit = document.querySelector('.pop-up__description-edit');
-let nameEdit = document.querySelector('.pop-up__name-edit');
-let form = document.querySelector('.pop-up__form');
-let popUpField = document.querySelector('.pop-up__field');
+let popUp = document.querySelector(".pop-up");
+let descriptionEdit = document.querySelector(".pop-up__edit_description");
+let nameEdit = document.querySelector(".pop-up__edit_name");
+let form = document.querySelector(".pop-up__form");
+let popUpField = document.querySelector(".pop-up__field");
 
 // FUNCTIONS
 // POP-UP
 // активация попапа
-editButton.addEventListener('click', function(){
-  popUp.classList.add('pop-up_type_active');
+editButton.addEventListener("click", function () {
+  popUp.classList.add("pop-up_type_active");
   nameEdit.value = profileName.textContent;
   descriptionEdit.value = profileDescription.textContent;
 });
 // дизактивация попапа
-function close(){;
-  popUp.classList.remove('pop-up_type_active');
-};
-saveButton.addEventListener('submit', save);
-closeButton.addEventListener('click', close);
+function close() {
+  popUp.classList.remove("pop-up_type_active");
+}
+closeButton.addEventListener("click", close);
 
 // сохранение
-function save(evt){
+function save(evt) {
   evt.preventDefault();
   profileName.textContent = nameEdit.value;
   profileDescription.textContent = descriptionEdit.value;
 
-  profileName.value = '';
-  profileDescription.value = '';
+  profileName.value = "";
+  profileDescription.value = "";
 
   close();
-};
-form.addEventListener('submit', save);
+}
+form.addEventListener("submit", save);
